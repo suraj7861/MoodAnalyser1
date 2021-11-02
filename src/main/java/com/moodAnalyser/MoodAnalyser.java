cd3ff62c77f155caab1dea0a6c11475c12a543ac
unknown <khomanesuraj8@gmail.com>
2021-11-02 20:14:47 +0530

    Analyse mood Whether it is Happy or Sad

--- src/main/java/com/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/moodAnalyser/MoodAnalyser.java
@@ -4,6 +4,21 @@ public class MoodAnalyser {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Mood Analyser Program");
+		//create object
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		//pass Happy message 
+		String message = moodAnalyser.analyseMood("This is Happy message");
+		System.out.println(message);
+		//pass Sad message 
+		message = moodAnalyser.analyseMood("This is Sad message");
+		System.out.println(message);
 	}
-
+	//check mood whether Happy or Sad
+	private String analyseMood(String message) {
+		if (message.contains("Sad")) {
+			return "Sad";
+		} else {
+			return "Happy";
+		}
+	}
 }
