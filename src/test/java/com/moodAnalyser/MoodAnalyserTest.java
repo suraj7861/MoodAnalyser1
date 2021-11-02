a232fa590695fcf0fd5a3b4d88191803e1b7a7b5
unknown <khomanesuraj8@gmail.com>
2021-11-02 20:25:08 +0530

    Test case to message should return Happy

--- src/test/java/com/moodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodAnalyser/MoodAnalyserTest.java
@@ -4,7 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
+	
+	// -----------------test for Happy-----------
 	@Test
 	public void givenMessageWhenSadShouldBeReturnSad() {
 		// create object
@@ -14,7 +15,17 @@ public class MoodAnalyserTest {
 		String expectedValue = "SAD";
 		String mood = moodeAnalyser.analyseMood(message);
 		Assert.assertEquals(expectedValue, mood);
-
 	}
 
+	// ---------------test for Happy---------------
+	@Test
+	public void givenMessageWhenHappyShouldBeReturnHappy() {
+		// create object
+		MoodAnalyser moodeAnalyser = new MoodAnalyser();
+		// pass Sad message
+		String message = "I am in Happy Mood";
+		String expectedValue = "HAPPY";
+		String mood = moodeAnalyser.analyseMood(message);
+		Assert.assertEquals(expectedValue, mood);
+	}
 }
