7f4ab8d7c2690a2b4c19c7d603de9ce1e8955af8
unknown <khomanesuraj8@gmail.com>
2021-11-02 20:12:18 +0530

    welcome message printed

--- src/main/java/com/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/moodAnalyser/MoodAnalyser.java
@@ -3,8 +3,7 @@ package com.moodAnalyser;
 public class MoodAnalyser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome To Mood Analyser Program");
 	}
 
 }
